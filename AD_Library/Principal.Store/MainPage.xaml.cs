9719eb717e9b55af2f39c7a04af2ee7cb3ee1f25
ivan espinal <iespinal@bnb.com.bo>
2021-08-16 23:54:56 -0400

    First commit develop

--- AD_Library/Principal.Store/MainPage.xaml.cs
+++ AD_Library/Principal.Store/MainPage.xaml.cs
@@ -33,6 +33,7 @@ namespace Principal.Store
         /// property is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            // Hola MUndo desde develop
         }
     }
 }
